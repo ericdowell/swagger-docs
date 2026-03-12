f0a6303b5ff487ccc928144784ad218a3739a890
Eric Dowell <github@ericdowell.com>
2026-03-12 18:59:42 -0500

    Adding OpenApi\Attributes to controllers.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,30 @@
 
 namespace App\Http\Controllers;
 
+
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: '1.0.0',
+    description: 'Backend API documentation for Example.',
+    title: 'Example API',
+)]
+#[OA\Server(
+    url: L5_SWAGGER_CONST_HOST,
+    description: 'API Server'
+)]
+#[OA\Components(
+    securitySchemes: [
+        new OA\SecurityScheme(
+            securityScheme: 'sanctum',
+            type: 'apiKey',
+            name: 'Authorization',
+            in: 'header'
+        ),
+    ]
+)]
+#[OA\Tag(name: 'Auth', description: 'Auth endpoints')]
+#[OA\Tag(name: 'User', description: 'User endpoints')]
 abstract class Controller
 {
     //
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use OpenApi\Attributes as OA;
+
+class UserController extends Controller
+{
+
+    #[OA\Get(
+        path: '/api/users/me',
+        operationId: 'currentUser',
+        description: 'Retrieves the currently authenticated user\'s profile information, including their avatar and other details.',
+        summary: 'Get the currently authenticated user',
+        security: [['sanctum' => []]],
+        tags: ['User'],
+        responses: [
+            new OA\Response(
+                ref: '#/components/responses/UserFeatureFlagsResponse',
+                response: 200,
+            ),
+            new OA\Response(
+                ref: '#/components/responses/Unauthenticated',
+                response: 401,
+            ),
+        ]
+    )]
+    public function me(Request $request): JsonResponse
+    {
+        $user = $request->user();
+
+        return response()->json(compact('user'));
+    }
+}
